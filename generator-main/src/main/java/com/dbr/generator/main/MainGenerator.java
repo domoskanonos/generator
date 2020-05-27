8e2e48d0a3d5fb75d607f515156f1403904f6930
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-05-27 13:32:47 +0200

    modified

--- generator-main/src/main/java/com/dbr/generator/main/MainGenerator.java
+++ generator-main/src/main/java/com/dbr/generator/main/MainGenerator.java
@@ -2,29 +2,72 @@ package com.dbr.generator.main;
 
 import com.dbr.generator.basic.util.GeneratorUtil;
 import com.dbr.generator.main.model.MainGeneratorModel;
+import com.dbr.util.SystemUtil;
+import com.dbr.util.ZipUtil;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class MainGenerator {
 
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         MainGeneratorModel mainGeneratorModel = new MainGeneratorModel();
+        mainGeneratorModel.setTempFolder(new File(System.getProperty("java.io.tmpdir"), "generator"));
         mainGeneratorModel.setRootFolder(new File("C:\\_dev"));
         mainGeneratorModel.setTechnicalDescriptor("ocivap");
+        mainGeneratorModel.setUseSpringBootTemplate(true);
         MainGenerator mainGenerator = new MainGenerator();
         mainGenerator.generate(mainGeneratorModel);
     }
 
-    public void generate(MainGeneratorModel model) {
+    public void generate(MainGeneratorModel model) throws IOException, InterruptedException {
         logger.info("generate project start...");
+        GeneratorUtil.makeDir(model.getTempFolder());
         GeneratorUtil.makeDir(model.getRootFolder());
         GeneratorUtil.makeDir(model.getProjectFolder());
+
+        if (model.getUseSpringBootTemplate()) {
+            File springBootZipFile = copyUrlToTempFolder(model.getSpringBootTemplateZipUrl(), model.getSpringBootTemplateZipFile());
+            unzipFile(springBootZipFile, model.getTempFolder());
+            createMavenArchetype(model.getSpringBootTemplateFolder());
+        }
+
         logger.info("generate project end...");
 
+    }
+
+    private void createMavenArchetype(File folder) throws IOException, InterruptedException {
+        logger.info("execute command, path: {}", folder.getAbsolutePath());
+        try {
+            Process process = Runtime.
+                    getRuntime().
+                    exec("cmd /c start \"\" create-maven-archetype.bat",null,folder);
+            System.out.println(process.getInputStream());
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void unzipFile(File file, File destination) throws IOException {
+        logger.info("unzip file, file: {}", file.getAbsolutePath());
+        ZipUtil.unzipFile(file, destination);
+    }
+
+    private File copyUrlToTempFolder(String url, File destination) throws IOException {
+        logger.info("copy url to temp folder, url: {}", url);
+        FileUtils.copyURLToFile(
+                new URL(url),
+                destination,
+                2000,
+                2000);
+        return destination;
     }
 
 
--- generator-main/src/main/java/com/dbr/generator/main/model/MainGeneratorModel.java
+++ generator-main/src/main/java/com/dbr/generator/main/model/MainGeneratorModel.java
@@ -7,11 +7,26 @@ import java.io.File;
 @Data
 public class MainGeneratorModel {
 
+    private File tempFolder;
+
     private File rootFolder;
 
     private String technicalDescriptor;
 
+    private Boolean useSpringBootTemplate = false;
+    private String springBootTemplateZipUrl = "https://github.com/domoskanonos/spring-boot-template/archive/master.zip";
+    private String springBootTemplateFilename = "spring-boot-template-master";
+    private String springBootTemplateZipFilename = new StringBuilder().append(springBootTemplateFilename).append(".zip").toString();
+
     public File getProjectFolder() {
         return new File(rootFolder, technicalDescriptor);
+    }
+
+    public File getSpringBootTemplateFolder() {
+        return new File(this.tempFolder, this.springBootTemplateFilename);
+    }
+
+    public File getSpringBootTemplateZipFile() {
+        return new File(this.tempFolder, getSpringBootTemplateZipFilename());
     }
 }
