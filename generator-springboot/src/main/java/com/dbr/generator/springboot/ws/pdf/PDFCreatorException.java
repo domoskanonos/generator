9d2982085d1752f8b7b3a894d679f2d04190a424
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-05-29 15:43:07 +0200

    modified

--- generator-springboot/src/main/java/com/dbr/generator/springboot/ws/pdf/PDFCreatorException.java
+++
@@ -1,9 +0,0 @@
-package com.dbr.generator.springboot.ws.pdf;
-
-public class PDFCreatorException extends RuntimeException {
-
-    public PDFCreatorException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
