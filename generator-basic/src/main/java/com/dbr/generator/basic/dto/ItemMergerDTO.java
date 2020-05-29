e40fba045c4c319a2f2a6f864a50a499d0441f84
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-05-29 13:42:59 +0200

    modified

--- generator-basic/src/main/java/com/dbr/generator/basic/dto/ItemMergerDTO.java
+++ generator-basic/src/main/java/com/dbr/generator/basic/dto/ItemMergerDTO.java
@@ -12,7 +12,7 @@ public class ItemMergerDTO {
     }
 
     public String getJavaMappingClazzSimpleName() {
-        if (this.objects.length > 1) {
+        if (this.objects.length < 2) {
             throw new RuntimeException("not enough objects for mapping class, minimum is 2 items.");
         }
         return new StringBuilder().append(this.objects[0].getJavaClazzSimpleName()).append(this.objects[1].getJavaClazzSimpleName()).append("Mapping").toString();
