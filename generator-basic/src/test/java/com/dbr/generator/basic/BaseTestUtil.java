72bd0451a1b5c169d699a675e470518b8327da57
dominik.bruhn <dominikbruhn@googlemail.com>
2020-05-30 15:06:46 +0200

    modified

--- generator-basic/src/test/java/com/dbr/generator/basic/BaseTestUtil.java
+++ generator-basic/src/test/java/com/dbr/generator/basic/BaseTestUtil.java
@@ -2,13 +2,10 @@ package com.dbr.generator.basic;
 
 import com.dbr.generator.basic.project.dto.ProjectDTO;
 
-import java.io.File;
-
 public class BaseTestUtil {
     public static ProjectDTO projectDTO = new ProjectDTO();
 
     static {
         projectDTO.setJavaBasePackage("com.dbr.generator.test");
-        projectDTO.setRootFolder(new File(""));
     }
 }
