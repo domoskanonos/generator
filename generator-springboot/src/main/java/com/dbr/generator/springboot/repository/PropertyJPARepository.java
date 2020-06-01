18fc8a475f6667f0353a9623ee2532b5e1c33123
dominik.bruhn <dominikbruhn@googlemail.com>
2020-06-01 17:09:48 +0200

    modified

--- generator-springboot/src/main/java/com/dbr/generator/springboot/repository/PropertyJPARepository.java
+++ generator-springboot/src/main/java/com/dbr/generator/springboot/repository/PropertyJPARepository.java
@@ -1,4 +1,12 @@
 package com.dbr.generator.springboot.repository;
 
-public class PropertyJPARepository {
+import com.dbr.generator.basic.entity.Property;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PropertyJPARepository extends JpaRepository<Property, Long> {
+
 }
+
