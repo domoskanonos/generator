af01567a9dc5bae23bf179608f1fe657837b80f2
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-06-02 15:46:50 +0200

    modified

--- generator-springboot/src/main/java/com/dbr/generator/springboot/system/listener/InitialDataLoader.java
+++ generator-springboot/src/main/java/com/dbr/generator/springboot/system/listener/InitialDataLoader.java
@@ -1,5 +1,12 @@
 package com.dbr.generator.springboot.system.listener;
 
+import com.dbr.generator.basic.converter.JavaClass2ItemDTOConverter;
+import com.dbr.generator.basic.dto.ItemDTO;
+import com.dbr.generator.basic.entity.Item;
+import com.dbr.generator.basic.merger.ItemTemplates;
+import com.dbr.generator.springboot.app.mapping.ItemItemDTOMapping;
+import com.dbr.generator.springboot.app.repository.ItemJPARepository;
+import com.dbr.generator.springboot.app.repository.PropertyJPARepository;
 import com.dbr.generator.springboot.system.ApplicationProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -11,9 +18,22 @@ public class InitialDataLoader implements ApplicationListener<ContextRefreshedEv
 
     @Autowired
     private ApplicationProperties applicationProperties;
+    @Autowired
+    private PropertyJPARepository propertyJPARepository;
+
+    @Autowired
+    private ItemJPARepository itemJPARepository;
+
+    @Autowired
+    ItemItemDTOMapping itemItemDTOMapping;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
+        ItemDTO itemDTO = new JavaClass2ItemDTOConverter().convert("", ItemTemplates.DTO_TEMPLATE, ItemDTO.class);
+        Item item = itemItemDTOMapping.toEntity(itemDTO);
+        item = itemJPARepository.save(item);
+
+
     }
 
 }
