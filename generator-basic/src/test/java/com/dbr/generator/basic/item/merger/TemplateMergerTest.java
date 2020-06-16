8832178aedba43d42a4a087e611d75f0f7f3b8b0
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-06-16 15:22:41 +0200

    modified

--- generator-basic/src/test/java/com/dbr/generator/basic/item/merger/TemplateMergerTest.java
+++ generator-basic/src/test/java/com/dbr/generator/basic/item/merger/TemplateMergerTest.java
@@ -1,10 +1,10 @@
 package com.dbr.generator.basic.item.merger;
 
-import com.dbr.generator.GeneratorProjectMetaData;
 import com.dbr.generator.basic.enumeration.PropertyType;
-import com.dbr.generator.basic.merger.TemplateMerger;
 import com.dbr.generator.basic.enumeration.Template;
+import com.dbr.generator.basic.merger.TemplateMerger;
 import com.dbr.generator.basic.model.ItemModel;
+import com.dbr.generator.basic.model.project.ProjectModel;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +19,7 @@ public class TemplateMergerTest {
 
     @Test
     public void mergeAllTemplates() throws IOException {
-        ItemModel item = new ItemModel(GeneratorProjectMetaData.SPRING_BOOT_JAVA_PROJECT_MODEL, "Item", PropertyType.LONG, Template.values());
+        ItemModel item = new ItemModel(new ProjectModel(), "Item", PropertyType.LONG, Template.values());
 
         for (Template template : item.getTemplate()) {
             String content = new TemplateMerger().create(template, item);
