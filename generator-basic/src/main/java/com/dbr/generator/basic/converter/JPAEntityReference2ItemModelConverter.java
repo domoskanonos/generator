803b14d15bceee058fd777d76ea61378091319fe
dominik.bruhn <dominik.bruhn@de.rhenus.com>
2020-06-23 15:50:51 +0200

    modified

--- generator-basic/src/main/java/com/dbr/generator/basic/converter/JPAEntityReference2ItemModelConverter.java
+++ generator-basic/src/main/java/com/dbr/generator/basic/converter/JPAEntityReference2ItemModelConverter.java
@@ -15,18 +15,33 @@ import java.util.List;
 public class JPAEntityReference2ItemModelConverter {
 
     public ItemModel convert(ProjectModel projectModel, DatabaseJPAUtil.JPAEntityReference jpaEntityReference, Template... template) {
-        ItemModel itemModel = new ItemModel(projectModel, jpaEntityReference.getJavaClassName(), PropertyType.byJavaTypeSimpleName(jpaEntityReference.getIdColumn().getJavaTypeSimpleName()), template);
+        PropertyType idPropertyType = PropertyType.byJavaTypeSimpleName(jpaEntityReference.getIdColumn().getJavaTypeSimpleName());
+        ItemModel itemModel = new ItemModel(projectModel, jpaEntityReference.getJavaClassName(), idPropertyType, template);
         itemModel.getTemplate().addAll(Arrays.asList(template));
+        boolean noMainProperty = true;
         for (DatabaseJPAUtil.ColumnReference columnReference : jpaEntityReference.getColumnReferences()) {
             PropertyModel propertyModel = new PropertyModel(itemModel, columnReference.getJavaFieldName());
             propertyModel.setUseJPAIdClazz(itemModel.getIdPropertyType() != null);
             propertyModel.setPropertyType(PropertyType.byJavaTypeSimpleName(columnReference.getJavaTypeSimpleName()));
-            propertyModel.setMainProperty(true);
+            if (jpaEntityReference.isIdColumn(columnReference) && PropertyType.STRING.equals(idPropertyType)) {
+                propertyModel.setMainProperty(true);
+                noMainProperty = false;
+            }
             propertyModel.setIdProperty(jpaEntityReference.isIdColumn(columnReference));
             propertyModel.setPropertyTypeSimpleName(columnReference.getJavaTypeSimpleName());
             propertyModel.setPropertyTypeName(columnReference.getJavaTypeSimpleName());
             itemModel.addProperty(propertyModel);
         }
+
+        if (noMainProperty) {
+            for (PropertyModel propertyModel : itemModel.getProperties()) {
+                if (PropertyType.STRING.equals(propertyModel.getPropertyType())) {
+                    propertyModel.setMainProperty(true);
+                    break;
+                }
+            }
+        }
+
         return itemModel;
     }
 
